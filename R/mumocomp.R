14c26b399d050bd4bb873f98c596649af4fa2626
daniel-gerhard <00gerhard@gamil.com>
2016-07-01 13:41:33 +1200

    changed mumotest() to mumocomp()

--- R/mumocomp.R
+++ R/mumocomp.R
@@ -1,15 +1,24 @@
-mumotest <-
+mumocomp <-
 function(mlist, K, B){
+  # number of obs (same for every model)
   n <- nrow(model.matrix(mlist[[1]]))
+  # evaluate score functions using sandwich
   psilist <- lapply(mlist, function(fm) t(bread(fm) %*% t(estfun(fm))))
-  psi <- t(matrix(unlist(psilist), nrow=n))
+  # contrast coefficient estimates
   est <- as.vector(K %*% unlist(lapply(mlist, function(fm) coef(summary(fm))[,1])))
+  # sandwich covariance of parameters within and between models
+  psi <- t(matrix(unlist(psilist), nrow=n))
   covorig <- ((psi %*% t(psi)) / n)
-  covar <- K %*% covorig %*% t(K)
-  stat <- est / (sqrt(diag(covar)) * (1/sqrt(n)))
   corr <- cov2cor(covorig)
+  # covariance of parameter linear combinations
+  covar <- K %*% covorig %*% t(K)
+  # test statistic
+  stat <- est / (sqrt(diag(covar)) * (1/sqrt(n)))
+  # resampling of test statistic
   bs <- cmr(psi, corr, n, K, B)
+  # maximum test statistics
   bmax <- apply(bs, 1, function(x) max(abs(x)))
+  # adjusted p-values
   alter <- sapply(stat, function(x) bmax > abs(x))
   pv <- apply(alter, 2, function(x) (sum(x) + 1)/(length(x) + 1))
   return(pv)
