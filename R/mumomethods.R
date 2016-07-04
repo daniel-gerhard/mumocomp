7bfab6fe3c50e856448ce89124d59f0b2875926e
daniel-gerhard <00gerhard@gamil.com>
2016-07-04 13:20:49 +1200

    print methods for confint and summary output

--- R/mumomethods.R
+++ R/mumomethods.R
@@ -13,13 +13,19 @@ summary.mumo <- function(object, digits= max(3, getOption("digits") - 3), ...){
   alter <- sapply(object$statistics, function(x) bmax > abs(x))
   pv <- apply(alter, 2, function(x) (sum(x) + 1)/(length(x) + 1))
   
-  cat("\t Multiple Model Contrast Tests\n\n") 
   dd <- data.frame(Estimate=object$coefficients,
                    Std.Err=sqrt(diag(object$vcov)/object$N),
                    Statistic=object$statistics,
-                   pvalue=pv,
-                   row.names=object$names)
-  printCoefmat(dd, digits=digits, cs.ind=1:2, tst.ind=3, has.Pvalue=TRUE) 
+                   pvalue=pv)
+  rownames(dd) <- object$names
+  out <- list(mumo=object, results=dd)
+  class(out) <- "summary.mumo"
+  return(out)
+}
+
+print.summary.mumo <- function(x, digits= max(3, getOption("digits") - 3), ...){
+  cat("\t Multiple Model Contrast Tests\n\n") 
+  printCoefmat(x$results, digits=digits, cs.ind=1:2, tst.ind=3, has.Pvalue=TRUE) 
 }
 
 
@@ -29,10 +35,19 @@ confint.mumo <- function(object, parm, level = 0.95, ...){
   upp <- object$coefficients + quant * sqrt(diag(object$vcov)/object$N) 
   low <- object$coefficients - quant * sqrt(diag(object$vcov)/object$N) 
   
-  cat(paste("\t Simultaneous", level, "Confidence Intervals\n\n")) 
+  
   cid <- data.frame(Estimate=object$coefficients,
                     lower=low,
-                    upper=upp,
-                   row.names=object$names)
-  return(cid)
+                    upper=upp)
+  rownames(cid) <- object$names
+  out <- list(mumo=object, 
+              level=level,
+              confint=cid)
+  class(out) <- "confint.mumo"
+  return(out)
+}
+
+print.confint.mumo <- function(x, digits=max(3, getOption("digits") - 3), ...){
+  cat(paste("\t Simultaneous", x$level, "Confidence Intervals\n\n")) 
+  print(x$confint, digits=digits, ...) 
 }
